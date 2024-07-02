b61a1223719fc778fb7d4d0f0df8d2df8f3e6934
peter <peter.rono@student.moringaschool.com>
2024-07-02 12:16:28 +0300

    Fix scope and function declarations for customer management

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+
+var customerName = 'bob';
+
+// Function to uppercase customerName
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+// Function to set bestCustomer
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+// Correct function name to overwrite bestCustomer
+function overwriteBestCustomer(newCustomer) {
+    bestCustomer = newCustomer;
+}
+
+// Declare leastFavoriteCustomer as a constant
+const leastFavoriteCustomer = 'some initial value';
+
+// Function attempting to change leastFavoriteCustomer (this will throw an error)
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'new';
+}
+
+
